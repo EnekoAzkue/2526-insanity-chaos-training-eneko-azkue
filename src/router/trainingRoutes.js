1c8c8346dfad7ab70c3bd86a88efbf03ff224c5d
EnekoAzkue <eneko.azkue@ikasle.aeg.eus>
2026-02-05 13:29:18 +0100

    [ADD]: create training route

--- src/router/trainingRoutes.js
+++ src/router/trainingRoutes.js
@@ -5,4 +5,6 @@ const trainingController = require('../controller/trainingsController');
 
 router.get('/all', trainingController.getAllTrainings)
 
+router.post('/post', trainingController.createTraining)
+
 module.exports = router;
