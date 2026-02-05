208f8d79cc70526c7ca567d2ca0712a08ea4d0ee
EnekoAzkue <eneko.azkue@ikasle.aeg.eus>
2026-02-05 13:27:20 +0100

    [ADD]: createTraining service

--- src/service/trainingService.js
+++ src/service/trainingService.js
@@ -1,24 +1,25 @@
 const Training = require('../database/trainings');
 
-const getAllTrainings = async() => {
-    try {
-        const allTrainings = Training.getAllTrainings();
-        return allTrainings
-    } catch (error) {
-        throw error
-    }
+
+const getAllTrainings = async () => {
+  try {
+    const allTrainings = Training.getAllTrainings();
+    return allTrainings
+  } catch (error) {
+    throw error
+  }
 }
 
-// const createTraining = async(newTraining) => {
-//     try {
-//         const createdTraining = Training.createTraining(newTraining);
-//         return createdTraining;
-//     } catch (error) {
-//         throw error
-//     }
-// }
+const createTraining = async (trainingState) => {
+  try {
+    const training = Training.createTraining(trainingState);
+    return training
+  } catch (error) {
+    throw error
+  }
+}
 
 module.exports = {
-    getAllTrainings,
-    // createTraining,
+  getAllTrainings,
+  createTraining,
 }
