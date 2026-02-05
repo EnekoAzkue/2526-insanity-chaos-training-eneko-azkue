0b8faa36a1c72c3eec07e02fffd07456a1e2f295
EnekoAzkue <eneko.azkue@ikasle.aeg.eus>
2026-02-05 13:28:41 +0100

    [ADD]: create training controller

--- src/controller/trainingsController.js
+++ src/controller/trainingsController.js
@@ -1,23 +1,39 @@
 const trainingService = require('../service/trainingService');
 
-const getAllTrainings = async (req, res) => {
-    try {
-        const allTrainings = await trainingService.getAllTrainings();
-        if (allTrainings.length === 0) {
-            return res.status(404).send({ message: "No existen trainings" })
-        }
-        res.send({ status: "OK", data: allTrainings })
-    } catch (error) {
-        res
-            .status(error?.status || 500)
-            .send({
-                status: "FAILED",
-                message: "Error al realizar la peticion:",
-                data: { error: error?.message || error }
-            });
+const getAllTrainings = async (res) => {
+  try {
+    const allTrainings = await trainingService.getAllTrainings();
+    if (allTrainings.length === 0) {
+      return res.status(404).send({ message: "No existen trainings" })
     }
+    res.send({ status: "OK", data: allTrainings })
+  } catch (error) {
+    res
+      .status(500)
+      .send({
+        status: "FAILED",
+        message: "Error al realizar la peticion:",
+        data: { error: error?.message || error }
+      });
+  }
 };
 
+const createTraining = async (res) => {
+  try {
+    const createdQuest = await trainingService.createTraining()
+    res.send({ data: createdQuest })
+  } catch (error) {
+    res
+      .status(500)
+      .send({
+        status: "FAILED",
+        message: "Error al realizar la peticion:",
+        data: { error: error?.message || error }
+      });
+  }
+}
+
 module.exports = {
-    getAllTrainings,
+  getAllTrainings,
+  createTraining,
 }
